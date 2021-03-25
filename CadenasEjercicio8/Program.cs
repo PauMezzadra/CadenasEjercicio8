aa8ad6cce89c74a14147e58bc9639acd1fe11c15
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 15:01:57 -0300

    Add project files.

---
+++ CadenasEjercicio8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenasEjercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase;
+            string cadena = "FIN";
+            do
+            {
+                Console.WriteLine("Ingrese cualquier frase, yo le avisaré cuando su frase contenga la cadena ´FIN´");
+                frase = Console.ReadLine().ToUpper();
+                if (frase == "")
+                {
+                    Console.WriteLine("Debe ingresar al menos una palabra");
+                }
+                else
+                {
+                    if (frase.Contains(cadena))
+                    {
+                        Console.WriteLine("Su frase contiene la cadena ´FIN´");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Su frase no contiene la cadena ´FIN´");
+                    }
+                }
+                
+            } while (!frase.Contains(cadena));
+
+
+            Console.ReadKey();
+        }
+    }
+}
